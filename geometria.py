58d65349fb7761a3cddcb508997395f15c9128a4
Renmonsan <Renato.m.s.fi@gmail.com>
2026-05-26 22:16:11 -0300

    Create geometria.py

---
+++ geometria.py
@@ -0,0 +1,22 @@
+def square(l:float):
+    '''Função que calcula a área de um quadrado'''
+    a = l**2
+    return a
+
+def retangulo(b:float,h:float):
+    '''Função que calcula a área de um retângulo'''
+    a = b * h
+    return a
+def areacirculo(r:float):
+    '''Função que calcula a área de um circulo'''
+    a = 3.14 * r**2
+    return a
+
+def compcirculo(r:float):
+    '''Função que calcula o comprimento de um circulo'''
+    a = 2 * 3.14 * r
+    return a
+def volcilind(r:float,h:float):
+    '''Função que calcula o volume de um cilindro'''
+    v = 3.1416 * r**2 * h
+    return v
